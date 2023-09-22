b82d13717719e8294f48307e108e622ba932e493
nihclil <oxx921@gmail.com>
2023-09-22 15:09:08 +0800

    add member system to attraction page

--- static/attraction.js
+++ static/attraction.js
@@ -179,3 +179,167 @@ radios.forEach((radio) => {
       : (money.textContent = "新台幣 2500 元");
   });
 });
+
+//開啟/關閉登入視窗
+const headerNavSignIn = document.querySelector(".header-nav__signin");
+const modalSignIn = document.querySelector(".modal-signin");
+const signInClose = document.querySelector(".signin__close");
+const signUpPrompt = document.querySelector(".signup__prompt");
+
+headerNavSignIn.addEventListener("click", showModalSignIn);
+
+signInClose.addEventListener("click", hideModalSignIn);
+
+signUpPrompt.addEventListener("click", function () {
+  showModalSignIn(), hideModalSignUp();
+});
+
+window.addEventListener("click", function (event) {
+  if (event.target === modalSignIn) {
+    hideModalSignIn();
+  }
+});
+
+function showModalSignIn() {
+  modalSignIn.style.display = "block";
+}
+
+function hideModalSignIn() {
+  modalSignIn.style.display = "none";
+}
+
+//登入表單
+const signInForm = document.querySelector(".signin__form");
+signInForm.addEventListener("submit", async function (event) {
+  event.preventDefault();
+  const email = document.querySelector(".signin__email").value;
+  const password = document.querySelector(".signin__password").value;
+
+  const data = await fetchSignIn(email, password);
+  if (data.token) {
+    localStorage.setItem("token", data.token);
+    window.location.reload();
+  } else {
+    const message = data.message;
+    displayMessage(message, ".signin__prompt");
+  }
+});
+
+//查詢會員帳號密碼
+async function fetchSignIn(email, password) {
+  const response = await fetch("http://34.225.182.0:3000/api/user/auth", {
+    method: "PUT",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({ email: email, password: password }),
+  });
+  return response.json();
+}
+
+//展示訊息
+function displayMessage(message, prompt) {
+  const modalPrompt = document.querySelector(prompt);
+  modalPrompt.textContent = message;
+}
+
+//開啟/關閉註冊視窗
+const signInPrompt = document.querySelector(".signin__prompt");
+const modalSignUp = document.querySelector(".modal-signup");
+const signUpClose = document.querySelector(".signup__close");
+
+signInPrompt.addEventListener("click", function () {
+  showModalSignUp(), hideModalSignIn();
+});
+
+signUpClose.addEventListener("click", hideModalSignUp);
+
+window.addEventListener("click", function (event) {
+  if (event.target === modalSignUp) {
+    hideModalSignUp();
+  }
+});
+
+function showModalSignUp() {
+  modalSignUp.style.display = "block";
+}
+
+function hideModalSignUp() {
+  modalSignUp.style.display = "none";
+}
+
+//註冊表單
+const signUpForm = document.querySelector(".signup__form");
+signUpForm.addEventListener("submit", async function (event) {
+  event.preventDefault();
+  const name = document.querySelector(".signup__name").value;
+  const email = document.querySelector(".signup__email").value;
+  const password = document.querySelector(".signup__password").value;
+  const data = await fetchSignUp(name, email, password);
+  console.log(data);
+  if (data.ok) {
+    displayMessage("註冊成功", ".signup__prompt");
+  } else {
+    const message = data.message;
+    displayMessage(message, ".signup__prompt");
+  }
+});
+
+//查詢信箱是否重複
+async function fetchSignUp(name, email, password) {
+  const response = await fetch("http://34.225.182.0:3000/api/user", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({ name: name, email: email, password: password }),
+  });
+  return response.json();
+}
+
+//檢查會員登入流程
+document.addEventListener("DOMContentLoaded", function () {
+  checkLoginStatus();
+  clickToLogOut(logout);
+});
+
+//按下登出按鈕
+function clickToLogOut(f) {
+  const actionButton = document.querySelector(".header-nav__signin");
+  actionButton.addEventListener("click", function () {
+    const isLogout = actionButton.textContent === "登出系統";
+    if (isLogout) {
+      f();
+    }
+  });
+}
+
+//清空Token
+function logout() {
+  localStorage.removeItem("token");
+  window.location.reload();
+}
+
+//確認token
+async function checkLoginStatus() {
+  const token = localStorage.getItem("token");
+  const response = await fetch("http://34.225.182.0:3000/api/user/auth", {
+    moethod: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const data = await response.json();
+  if (data) {
+    displayActionButton(data);
+  }
+}
+
+//登入/登出文字呈現
+function displayActionButton(data) {
+  const actionButton = document.querySelector(".header-nav__signin");
+  if (data.data) {
+    actionButton.textContent = "登出系統";
+  }
+}
